9e861acc96eb36a77838e6b2e0d8bc2426a09e77
naviNBRuas <enbeeare@outlook.com>
2025-10-15 14:40:01 -0300

    feat: Implement system-info module

--- examples/modules/system-info/main.go
+++ examples/modules/system-info/main.go
@@ -1,3 +1,46 @@
+//go:build wasip1
+
 package main
+
+import (
+	"encoding/json"
+	"runtime"
+	"unsafe"
+)
+
+//go:wasmimport apa_host log_message
+func log_message(ptr, size uint32)
+
+//export get_system_info
+func get_system_info() {
+	info := struct {
+		OS   string `json:"os"`
+		Arch string `json:"arch"`
+		CPUs int    `json:"cpus"`
+	}{
+		OS:   runtime.GOOS,
+		Arch: runtime.GOARCH,
+		CPUs: runtime.NumCPU(),
+	}
+
+	data, err := json.Marshal(info)
+	if err != nil {
+		return
+	}
+
+	ptr, size := stringToPtr(string(data))
+	log_message(ptr, size)
+}
+
+//export _start
+func _start() {
+	get_system_info()
+}
+
+func stringToPtr(s string) (uint32, uint32) {
+	buf := []byte(s)
+	ptr := &buf[0]
+	return uint32(uintptr(unsafe.Pointer(ptr))), uint32(len(buf))
+}
 
 func main() {}
