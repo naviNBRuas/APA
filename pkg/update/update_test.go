fa28c5c5084810531827eb17cc423e61fe4758bd
Navin B. Ruas <founder@nbr.company>
2026-06-30 01:16:59 +0000

    fix: remove unused testify/mock and libp2p/peer imports from update_test

--- pkg/update/update_test.go
+++ pkg/update/update_test.go
@@ -13,9 +13,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 	"golang.org/x/mod/semver"
 )
